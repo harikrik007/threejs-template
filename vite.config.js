f24672a3ab93ddcf0495175eec8a35625c6cc931
harikrik007 <iamharihk@gmail.com>
2026-03-02 14:22:01 +0530

    added vite config

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    server: {
+        host: true,
+        allowedHosts: 'all',
+        port: 5173,
+    },
+    build: {
+        outDir: 'dist',
+    },
+})
