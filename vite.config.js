af9ce72297b6b3f175abf3aec62eb00b9303e880
harikrik007 <iamharihk@gmail.com>
2026-03-02 14:33:32 +0530

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     server: {
         host: true,
-        allowedHosts: 'all',
+        allowedHosts: true,
         port: 5173,
     },
     build: {
